9c8fe4c5c33d9b79361a1cfdca521dad129bbc3b
dovara41 <dovara41@gmail.com>
2021-09-30 09:27:57 +0200

    1st commit

---
+++ Assets/Scripts/Brain.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour
+{
+
+    public static Brain self;
+
+    [SerializeField]
+    private GameObject platform;
+
+    // ---
+
+    private float yPos = 2;
+    private List<List<GameObject>> allLines = new List<List<GameObject>>();
+    
+    private void Awake()
+    {
+        self = this;
+
+        StartCoroutine(SpawnPlatformsOnStart());
+    }
+
+    private IEnumerator SpawnPlatformsOnStart()
+    {
+        yield return new WaitForSeconds(0.1f);
+        SpawnThreePlatforms();
+        yield return new WaitForSeconds(0.1f);
+        SpawnTwoPlatforms();
+        yield return new WaitForSeconds(0.1f);
+        SpawnThreePlatforms();
+        yield return new WaitForSeconds(0.1f);
+        SpawnTwoPlatforms();
+        yield return new WaitForSeconds(0.1f);
+        SpawnThreePlatforms();
+        yield return new WaitForSeconds(0.1f);
+        SpawnTwoPlatforms();
+    }
+
+    private void SpawnThreePlatforms()
+    {
+        allLines.Add(new List<GameObject> {
+            Instantiate(platform, new Vector3(0, yPos, 0), Quaternion.identity),
+            Instantiate(platform, new Vector3(-2, yPos, 0), Quaternion.identity),
+            Instantiate(platform, new Vector3(2, yPos, 0), Quaternion.identity)
+        });
+
+        yPos -= 1.5f;
+    }
+
+    private void SpawnTwoPlatforms()
+    {
+        allLines.Add(new List<GameObject> {
+            Instantiate(platform, new Vector3(1, yPos, 0), Quaternion.identity),
+            Instantiate(platform, new Vector3(-1, yPos, 0), Quaternion.identity)
+        });
+
+        yPos -= 1.5f;
+    }
+
+    public void DestroyUpperLine()
+    {
+        foreach (var platform in allLines[0])
+        {
+            Destroy(platform);
+        }
+        
+        allLines.RemoveAt(0);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(0, 1)]
+    private float fillAmount = 0;
+    [SerializeField]
+    private SpriteRenderer fill;
+    [SerializeField]
+    private TextMeshProUGUI timeText;
+
+    // ---
+
+    private float time = 10.0f;
+    private int currentXposition = 0;
+
+    private void Awake()
+    {
+        SettingsOnStart();
+    }
+
+    private void Update()
+    {
+        LifeTimer();
+        Movement();
+    }
+
+    private void OnValidate()
+    {
+        FillAmountChange();
+    }
+
+    void SettingsOnStart()
+    {
+
+    }
+
+    private void FillAmountChange()
+    {
+        fill.size = new Vector2(fill.size.x, fillAmount);
+        fill.transform.localPosition = new Vector3(fill.transform.localPosition.x, fillAmount / 2 - 0.5f, 0);
+    }
+
+    private void LifeTimer()
+    {
+        if (time <= 0)
+            return;
+
+        time -= Time.deltaTime;
+        timeText.text = time.ToString("F1");
+
+        if(time <= 10)
+        {
+            fill.size = new Vector2(fill.size.x, 1 - (time / 10));
+            fill.transform.localPosition = new Vector3(fill.transform.localPosition.x, (1 - (time / 10)) / 2 - 0.5f, 0);
+        }
+        else
+        {
+            fill.size = new Vector2(fill.size.x, 0);
+            fill.transform.localPosition = new Vector3(fill.transform.localPosition.x, 0, 0);
+        }
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) // Left
+        {
+            if (currentXposition == -2)
+                return;
+
+            transform.position = new Vector3(transform.position.x - 1, transform.position.y - 1.5f, 0);
+            currentXposition -= 1;
+            Brain.self.DestroyUpperLine();
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) // Right
+        {
+            if (currentXposition == 2)
+                return;
+
+            transform.position = new Vector3(transform.position.x + 1, transform.position.y - 1.5f, 0);
+            currentXposition += 1;
+            Brain.self.DestroyUpperLine();
+        }
+    }
+
+}
